97898e6f52576c0d20ec566b219d47b9afd13daa
EonEmmanuel <emmanueltresor027@gmail.com>
2026-03-10 15:29:09 +0100

    finalising create class form

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,6 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest"
 import {BACKEND_BASE_URL} from "@/constants";
-import {ListResponse} from "@/types";
+import {CreateResponse, ListResponse} from "@/types";
 import {HttpError} from "@refinedev/core";
 
 if(!BACKEND_BASE_URL) throw new Error(
@@ -63,6 +63,18 @@ const options: CreateDataProviderOptions = {
       const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
+    }
+  },
+
+  create: {
+    getEndpoint: ({resource}) => resource,
+
+    buildQueryParams: async ({ variables }) => variables,
+
+    mapResponse: async (response) => {
+      const json: CreateResponse = await response.json();
+
+      return json.data ?? [];
     }
   }
 }
